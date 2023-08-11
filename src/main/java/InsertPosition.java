c48c4215bc00ad001a69f21844e648bb6b9bd6c5
yuchen Zhao <yuchenzhao0620@gmail.com>
2023-08-11 05:58:15 +0000

    Lab Finished

--- src/main/java/InsertPosition.java
+++ src/main/java/InsertPosition.java
@@ -15,6 +15,18 @@ public class InsertPosition {
      * @return return the index that a number should be inserted into a sorted array.
      */
     public int searchInsert(int[] nums, int target) {
-        return -1;
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high){
+            int mid = low + ((high - low) / 2);
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] < target){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+        return low;
     }
 }
